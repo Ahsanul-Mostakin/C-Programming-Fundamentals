8694106e8ac3cee317acddc6cfe84a054d497e5f
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2024-10-21 08:28:09 +0600

    Add basic arithmetic operations example

---
+++ VS_Code_Editor/demo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() {
+    int a = 10, b = 10, c;
+
+    c = a + b;
+    printf("a+b=%d\n", c);
+
+    c = a - b;
+    printf("a-b=%d\n", c);
+
+    c = a * b;
+    printf("a*b=%d\n", c);
+
+    c = a / b;
+    printf("a/b=%d\n", c);
+}
