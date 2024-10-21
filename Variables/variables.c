8663734ed0fa71ec0afc23f0eae9ee15063cf513
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2024-10-22 01:47:58 +0600

    Variables example for basic C data types

---
+++ Variables/variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int age = 25;        
+    float height = 5.9;          
+    char grade = 'A';       
+    double pi = 3.14;
+
+    printf("Age: %d\n", age);
+    printf("Height: %.1f\n", height);
+    printf("Grade: %c\n", grade);
+    printf("Value of Pi: %.10f\n", pi);
+
+    return 0;
+}
