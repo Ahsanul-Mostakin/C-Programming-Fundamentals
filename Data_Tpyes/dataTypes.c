a3677f543c6b625530aed69a3d63db03428e3281
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2024-10-21 08:58:13 +0600

    Add example for basic C data types

---
+++ Data_Tpyes/dataTypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    float floatingPoint = 5.5;    
+    int integer = 10;               
+    char character = 'A';            
+    double doublePrecision = 3.14159;
+
+    printf("Integer: %d\n", integer);
+    printf("Float: %.2f\n", floatingPoint);
+    printf("Character: %c\n", character);
+    printf("Double: %.5f\n", doublePrecision);
+
+}
